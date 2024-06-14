d5fa289aefbdd423e5914d449ca6f5f058535d08
Akshaysuresh07 <akshaysureshm010@gmail.com>
2024-06-14 15:22:09 +0530

    user

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token,'5645455' , (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
